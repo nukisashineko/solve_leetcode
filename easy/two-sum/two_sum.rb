f9a46fc495af8a9461a85bcb7ed0f158d85165d1
nukisashineko <nukisashineko@gmail.com>
2019-03-27 00:24:46 +0900

    solved two_sum

---
+++ easy/two-sum/two_sum.rb
@@ -0,0 +1,17 @@
+# https://leetcode.com/submissions/detail/217771848/
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    numberWithIndex = Struct.new(:num, :index, :hash)
+    nums_with_index = nums.map.with_index{|num, index| numberWithIndex.new(num, index, rand)}
+    hashed_nums_with_index = nums_with_index.map{|x| [x.num, x]}.to_h
+    pair_a = nums_with_index.lazy.select do |a|
+        pair_b = hashed_nums_with_index[target - a.num]
+        pair_b && pair_b.hash != a.hash
+    end.first
+    equal_target_combination = [pair_a, hashed_nums_with_index[target - pair_a.num]]
+    nums_indexes = equal_target_combination.map(&:index)
+    nums_indexes
+end
