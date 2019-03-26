b878ea0960b817ab0f0ea8636e4cef53fda5123c
nukisashineko <nukisashineko@gmail.com>
2019-03-27 00:53:22 +0900

    solved reverse-integer

---
+++ easy/reverse-integer/reverse_integer.rb
@@ -0,0 +1,22 @@
+# https://leetcode.com/submissions/detail/217778025/
+# Runtime: 40 ms
+# Memory Usage: 9.6 MB
+
+
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+    sign = (x > 0 ? 1: -1)
+    reversed_abs_x =
+        x.abs
+            .to_s.split(//).reverse
+            .drop_while{ |char| char === "0" }.join
+            .to_i
+
+    reversed_signed_x = sign * reversed_abs_x
+    if - 2 ** 31 <= reversed_signed_x && reversed_signed_x <= (2 ** 31 -1)
+        reversed_signed_x
+    else
+        0
+    end
+end
